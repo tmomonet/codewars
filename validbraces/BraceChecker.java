91c6cbba2fb7b90139d86716a61b7c7eca0600ed
tmomonet <thollins@cnm.edu>
2025-08-19 14:38:26 -0600

    Brace checker started in semi-pseudo code

--- validbraces/BraceChecker.java
+++ validbraces/BraceChecker.java
@@ -3,8 +3,30 @@
 public class BraceChecker {
 
   public boolean isValid(String braces) {
-    // Add code here
-    return false;
+    char[] chars = braces.toCharArray();
+    HashMap<Character, Character> map = {
+        ')' : '(',
+        ']' : '[',
+        '}' : '{',
+    };
+
+    if (braces.length() % 2 != 0) {
+      return false;
+    }
+    Stack stack = Stack();
+    for (int i = 0; i < chars.length; i++) {
+        if (chars[i] == '(' || chars[i] == '[' || chars[i] == '{'){
+          stack.push(chars[i])
+        }
+        if (chars[i] == ')' || chars[i] == ']' || chars[i] == '}'){
+          if (stack.top == map.get(chars[i])){
+            stack.pop()
+          }
+        }
+    }
+    if (!stack.empty){
+      return false;
+    }
   }
 
 }
