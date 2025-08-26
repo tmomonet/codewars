6460f5af76f31bcfc21029c3296e4052d31c2580
Travis Hollins <87035520+tmomonet@users.noreply.github.com>
2025-08-26 15:55:13 -0600

    init commit

---
+++ convertcamel/Solution.java
@@ -0,0 +1,16 @@
+import java.lang.StringBuilder;
+import java.util.Arrays;
+
+class Solution{
+
+  static String toCamelCase(String s){
+    String regex = "[_-]";
+    String [] splitter = s.split(regex);
+    StringBuilder camelCase = new Builder;
+    System.out.println(Arrays.toString(splitter));
+    for (int i  = 0; i < splitter.length(); i++){
+      
+    }
+    return "";
+  }
+}
