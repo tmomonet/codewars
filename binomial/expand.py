63f4acd48b2fdcc1f735ebc421100ba5e3d21375
Travis Hollins <87035520+tmomonet@users.noreply.github.com>
2025-12-31 13:09:36 -0700

    Added Community Solution

--- binomial/expand.py
+++ binomial/expand.py
@@ -96,3 +96,25 @@ def expand(expr):
             output += term_to_str(coeff, a_var, i, b_var, j, is_first=(i == exponent))
 
     return output
+
+import re
+
+
+# Community Solution
+# P = re.compile(r'\((-?\d*)(\w)\+?(-?\d+)\)\^(\d+)')
+
+# def expand(expr):
+#     a,v,b,e = P.findall(expr)[0]
+    
+#     if e=='0': return '1'
+    
+#     o   = [int(a!='-' and a or a and '-1' or '1'), int(b)]
+#     e,p = int(e), o[:]
+    
+#     for _ in range(e-1):
+#         p.append(0)
+#         p = [o[0] * coef + p[i-1]*o[1] for i,coef in enumerate(p)]
+    
+#     res = '+'.join(f'{coef}{v}^{e-i}' if i!=e else str(coef) for i,coef in enumerate(p) if coef)
+    
+#     return re.sub(r'\b1(?=[a-z])|\^1\b', '', res).replace('+-','-')
